683df70f0e891bdffd429c2ada897052b25e447c
Stephen Sugden <me@stephensugden.com>
2014-09-01 00:32:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# ex-tensions
+
+Extensible and scoped `require.extensions`. 
+
+## Synopsis
+
+Require the `'ex-tensions'` module very early on in your source, like the first line:
+
+```javascript
+require('ex-tensions');
+```
+
+This patches `require.extensions` with a new method named `preprocess`.
+
+Let's imagine we've written a totally awesome compile-to-JS language that's
+"Just JavaScript", we'll call it JJScript, and it looks like this:
+
+```
+val CHEESE <- "brie 4 lyfe".
+val BEVERAGE <- "Weißbier".
+console!log["I want", BEVERAGE, "and", CHEESE, "!"].
+```
+
+(In case you're the type who doesn't like silly examples,  ¯\_(ツ)\_/¯).
+
+Here's our "compiler":
+
+```javascript
+function compileJJScript (module, content) {
+  // transform assignments
+  content = content.replace(/val ([A-Z]+) <- (.+)\/g, "var $1 = $2")
+  // transform brackets
+  content = content.replace(/\[/g, '(').replace(/\]/g, ')');
+  // transform statement terminators
+  content = content.replace(/\./, ';');
+  // transform property accessors
+  content = content.replace(/!/g, '.');
+
+  // replace file extension
+  module.filename = module.filename.replace(/\.jjs$/, '.js');
+
+  // return transformed source
+  return content;
+}
+```
+
+We hook that up to require.extensions like so:
+
+```
+require.extensions.preprocess('.jjs', __dirname, compileJJScript);
+```
+
+... and JJScript is now a legit language! when we require the .jjs source above we will run this javascript:
+
+```javascript
+var CHEESE = "brie 4 lyfe";
+var BEVERAGE = "Weißbier";
+console.log("I want", BEVERAGE, "and", CHEESE, "!");
+```
+
+## But... `require.extensions` already does that?
+
+Mostly, but notice how we're passing `__dirname` up there? That defines the "scope" for the extension we've just registered. What this means is the preprocessor will only be run for `.jjs` files that are in the same directory (or one of it's subdirectories) as the current file. _However_ files in `__dirname + '/node_modules'` will *not* be processed. This ability to scope preprocessors is one of two big features that `ex-tensions` brings, the next is...
+
+## Stacking Preprocessors
+
+Beyond silly party tricks, we can hook up useful compilers like [sweet.js](http://sweetjs.org/) or [regenerator](http://facebook.github.io/regenerator/), in fact we can stack them both:
+
+```javascript
+require.extensions.preprocess('.js', __dirname, regenerator);
+require.extensions.preprocess('.js', __dirname, compileSweetJS);
+```
+
+Multiple preprocessors are run in LIFO order: the above first compiles out macros with Sweet.js, then generators with regenerator. These transformations are scoped to our module, and run using the versions of each preprocessor we have installed locally.
+
+## Switching extensions
+
+The JJS compiler replaced the '.jjs' extension of `module.filename` with '.js'. What this does is signal `ex-tensions` that the source should now be processed by the '.js' preprocessor stack, so if our compiler produced e.g. valid generators, those would get compiled by regenerator.
+
+Another place this can be used is in generic transforms, such as reading code out of Markdown files:
+
+```
+require.extensions.preprocess('.md', __dirname, codeFromMarkdown);
+require.extensions.preprocess('.markdown', __dirname, codeFromMarkdown);
+
+function codeFromMarkdown (module, source) {
+  module.filename = module.filename.split('.').slice(0, -1).join('.');
+
+  return source.split('\n').filter(function (line) {
+    return /^    /.test(line);
+  }).map(function (line) {
+    return line.substr(4);
+  }).join('\n');
+}
+```
+
+The preprocessor above will work for filenames like "blah.jjs.md", "blah.coffee.markdown", or any other file extension we have a preprocessor for.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,147 @@
+var debug = require('debug')('extensions');
+var fs = require('fs');
+var path = require('path');
+
+var VERY_SECRET = '  super magic happy fun times  ';
+
+// Sentinel value that signifies a global preprocessor. This can only be used
+// within this module, so ignore it.
+var GLOBAL_SCOPE = {};
+
+// already patched? return early
+if (require.extensions[VERY_SECRET]) {
+  return false;
+}
+
+Object.defineProperty(require.extensions, 'preprocess', {
+  value: addPreprocessor
+});
+
+function addPreprocessor (ext, scope, processor) {
+  var loader = this[ext] || initLoader(this, ext);
+  if (!this[ext].unshift) {
+    throw new Error(
+      'Refusing to clobber existing require extension for ' + ext
+    );
+  }
+  loader.unshift(scope, processor);
+}
+
+function initLoader (extensions, ext) {
+  var stages = [];
+
+  var extRegexp = new RegExp(ext + '$');
+
+  Object.defineProperty(extensions, ext, {
+    enumerable: true,
+    get: function () {
+      return loader;
+    },
+    set: function () {
+      throw new TypeError('"Deprecated", use require.extensions["' +
+                          ext + '"].unshift(scope, contentProcessor)');
+    }
+  });
+
+  function loader (module, filename) {
+    var content = loader.preprocess(module, fs.readFileSync(filename, 'utf8'));
+    module._compile(content, filename);
+  }
+
+  loader.unshift = function (scope, processor) {
+    if (scope !== GLOBAL_SCOPE && typeof scope !== 'string') {
+      throw new TypeError('scope must be a string path, prefer __dirname');
+    }
+    if (typeof processor !== 'function') {
+      throw new TypeError('processor must be a function');
+    }
+    debug('added preprocessor %s for %s in %s',
+          processor.name || '<anonymous>', ext, scope);
+    if (scope === GLOBAL_SCOPE) {
+      stages.unshift(processor);
+    } else {
+      stages.unshift(scoped(scope, processor));
+    }
+  }
+
+  loader.preprocess = function (module, content) {
+    for (var i = 0, len = stages.length; i < len; i++) {
+      content = stages[i](module, content);
+      if (!extRegexp.test(module.filename)) {
+        // Previous stage changed extension of module.filename
+        // call the new loader.
+        console.log('extension changed %s', module.filename);
+        return chooseLoader(module.filename).preprocess(module, content);
+      }
+    }
+    return content
+  };
+
+  function chooseLoader (filename) {
+    var ext = '.' + filename.split('.').slice(1).pop();
+    if (require.extensions.hasOwnProperty(ext)) {
+      return require.extensions[ext];
+    }
+    throw new TypeError('No loader for extension ' + ext);
+  }
+
+  return loader;
+}
+
+/** Wrap a processor function so it will only be applied to the given scope */
+function scoped (scope, processor) {
+  var basedir = new RegExp('^' + path.resolve(scope));
+  var nm = new RegExp('/node_modules/');
+  return function (module, content) {
+    if (basedir.test(module.filename) &&
+        !nm.test(module.filename.replace(basedir, ''))
+    ) {
+      return processor(module, content);
+    } else {
+      return content;
+    }
+  }
+}
+
+Object.defineProperty(require.extensions, VERY_SECRET,
+                      { enumerable: false, value: true });
+
+/**
+ * Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
+ * because the buffer-to-string conversion in `fs.readFileSync()`
+ * translates it to FEFF, the UTF-16 BOM.
+ *
+ * Copied directly from joyent/node's lib/module.js
+ *
+ * @api private
+ */
+function stripBOM (module, content) {
+  if (content.charCodeAt(0) === 0xFEFF) {
+    content = content.slice(1);
+  }
+  return content;
+}
+
+// strip the shebang if present, again copied from joyent/node/lib/module.js
+function stripShebang (module, content) {
+  return content.replace(/^\#\!.*/, '');
+}
+
+function jsonToJavaScript (module, content) {
+  module.filename = module.filename.replace(/\.json$/, '.js');
+  try {
+    JSON.parse(content)
+  } catch (e) {
+    throw new Error(module.filename + ' is not valid JSON');
+  }
+  return 'module.exports=' + content + ';';
+}
+
+['.js', '.json'].forEach(function (ext) {
+  delete require.extensions[ext];
+  initLoader(require.extensions, ext);
+});
+
+require.extensions.preprocess('.js', GLOBAL_SCOPE, stripBOM);
+require.extensions.preprocess('.js', GLOBAL_SCOPE, stripShebang);
+require.extensions.preprocess('.json', GLOBAL_SCOPE, jsonToJavaScript);
